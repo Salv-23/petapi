485665a90f6ecc75a292e06ca6338417fdf0e0ce
Salv-23 <seodent.qro@gmail.com>
2021-12-22 13:28:13 -0600

    third commit

--- src/api/models.py
+++ src/api/models.py
@@ -4,10 +4,16 @@ from src import app, db
 from enum import Enum, unique
 
 
-# Enum type
+# user type for User
 class Types(str, Enum):
     veterinarian = 'veterinarian'
     owner = 'owner'
+
+# phone number type for PhoneNumbers
+class NumberType(str, Enum):
+    home = 'home'
+    work = 'work'
+    mobile = 'mobile'
 
 
 # models
@@ -18,19 +24,22 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    userTypes = db.Column(db.Enum(Types), nullable=False)
+    user_type = db.Column(db.Enum(Types), nullable=False)
     
-    # Relationships 1 to 1
+    # 1 to 1 relationships
     address = db.relationship('UserAddress', backref='user', uselist=False)
-    # Relationships 1 to many
-    verifcationPapers = db.relationship('VerificationPapers', backref='user')
+    vet_info = db.relationship('VeterinaryInformation', backref='user', uselist=False)
+    # 1 to many relationships
+    verifcation_papers = db.relationship('VerificationPapers', backref='user')
+    phone_numbers = db.relationship('PhoneNumbers', backref='user')
+    cards = db.relationship('Cards', backref='user')
     
 
-    def __init__(self, name, last_name, email, userTypes):
+    def __init__(self, name, last_name, email, user_type):
         self.name = name
         self.last_name = last_name
         self.email = email
-        self.userTypes = userTypes
+        self.user_type = user_type
 
 
 class UserAddress(db.Model):
@@ -40,16 +49,16 @@ class UserAddress(db.Model):
     address = db.Column(db.String(50), nullable=False)
     city = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(30), unique=True, nullable=False)  
-    zipCode = db.Column(db.Integer, nullable=False)
-    userAdress = db.Column(db.Integer, ForeignKey('user.id'))
+    zip_code = db.Column(db.Integer, nullable=False)
+    user_adress = db.Column(db.Integer, ForeignKey('user.id'))
 
 
 class VerificationPapers(db.Model):
     __tablename__ = 'verification_papers'
 
     id = db.Column(db.Integer, primary_key=True)
-    documentType = db.Column(db.String(100), nullable=False)
-    documentNumber = db.Column(db.Integer, nullable=False)
+    document_type = db.Column(db.String(100), nullable=False)
+    document_number = db.Column(db.Integer, nullable=False)
     owner = db.Column(db.Integer, ForeignKey('user.id'))
 
 
@@ -62,5 +71,26 @@ class VeterinaryInformation(db.Model):
     address = db.Column(db.String(50), nullable=False)
     verification_status = db.Column(db.Boolean, nullable=False)
     veterinarian = db.Column(db.Integer, ForeignKey('user.id'))
+
+
+class PhoneNumbers(db.Model):
+    __tablename__ = 'phone_numbers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(30), nullable=False)
+    number_type = db.Column(db.Enum(NumberType))
+    owner = db.Column(db.Integer, ForeignKey('user.id'))
+
+
+class Cards(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pet_name = db.Column(db.String(50), nullable=False)
+    pet_race = db.Column(db.String(30), nullable=False)
+    pet_gender = db.Column(db.String(30), nullable=False)
+    birthday = db.Column(db.DateTime)
+    notes = db.Column(db.String(500))
+    owner = db.Column(db.Integer, ForeignKey('user.id'))
 
 

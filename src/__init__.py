07970cd54dd78875e8e2eb3cd9ccb0472570ac91
Hector SB <hector.sab@hotmail.com>
2021-12-25 13:42:31 -0600

    fix(main): Initialize flask app from main.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,3 @@
-import os
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
-from flask_sqlalchemy import SQLAlchemy  
-
-
-# instantiate the app
-app = Flask(__name__)
-# instantiate the api
-api = Api(app)
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
-# instantiate the db
-db = SQLAlchemy(app)
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
+import os
+from flask import Flask, jsonify
 from flask.cli import FlaskGroup
-from . import app, db
+from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
-# extending the Flask CLI with our own 
+
+# Instantiate the app
+app = Flask(__name__)
+
+# Instantiate the api
+api = Api(app)
+
+# Set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# Instantiate the db
+db = SQLAlchemy(app)
+
+# Extending the Flask CLI with our own
 cli = FlaskGroup(app)
 
 # custom command to create and commit to db
